72885c663aa92a8feae1b598ae9bc1f5e54145db
SabrinaSusmi <sabrina.su512@gmail.com>
2020-08-07 01:52:55 +0600

    Player's move added to classic theme #5

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ClassicTheme.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ClassicTheme.java
@@ -2,17 +2,21 @@ package TicTacToe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClassicTheme {
     private JPanel panel;
     private JButton button[] = new JButton[9];
-    DefensiveAI defensiveAI = new DefensiveAI();
+    private Font font;
 
     public ClassicTheme(JPanel panel) {
         this.panel = panel;
     }
 
     public JPanel drawClassicTheme() {
+
+        font = new Font("Arial", Font.BOLD, 30);
 
         panel.setLayout(new GridLayout(3,3));
 
@@ -22,9 +26,21 @@ public class ClassicTheme {
             button[i].setBorder(BorderFactory.createLineBorder(Color.black,2));
             panel.add(button[i]);
         }
-        if(defensiveAI.firstMove(button)){
-            button[1].setText("X");
+
+        for (JButton button: button
+             ) {
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(button.getText()==""){
+                        button.setText("X");
+                        button.setFont(font);
+                    }
+                }
+            });
+
         }
+
 
         return panel;
     }
