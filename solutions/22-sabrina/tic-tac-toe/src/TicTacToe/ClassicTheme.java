69e058c20d481909822333030aa7065f0ceed9e2
SabrinaSusmi <sabrina.su512@gmail.com>
2020-08-07 03:31:04 +0600

    move attempt.  #5 #6

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ClassicTheme.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ClassicTheme.java
@@ -4,11 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class ClassicTheme {
     private JPanel panel;
     private JButton button[] = new JButton[9];
     private Font font = new Font("Arial", Font.BOLD, 30);
+    private String currentPlayer;
 
     public ClassicTheme(JPanel panel) {
         this.panel = panel;
@@ -24,8 +26,18 @@ public class ClassicTheme {
             panel.add(button[i]);
         }
 
+        currentPlayer = "x";
 
-        AIMove(button);
+        for(int i=0; i<9; i++){
+            if(currentPlayer=="x"){
+                playerMove(button);
+                currentPlayer="o";
+            }
+            else {
+                AIMove(button);
+                currentPlayer="x";
+            }
+        }
 
 
         return panel;
@@ -48,13 +60,15 @@ public class ClassicTheme {
     }
 
     private void AIMove(JButton button[]){
-        int index = (int)Math.random()%8;
-        if(button[index].getText()==""){
-            button[index].setText("O");
-            button[index].setFont(font);
+        int index = new Random().nextInt(9);
+        while(true){
+            if(button[index].getText()!=""){
+                index = new Random().nextInt(9);
+            }
+            else
+                break;
         }
-        else {
-            AIMove(button);
-        }
+        button[index].setText("O");
+        button[index].setFont(font);
     }
 }
