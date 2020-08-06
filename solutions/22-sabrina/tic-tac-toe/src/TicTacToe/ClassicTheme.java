75fc586ef062474bbd0282652a4d41eedfcf17d2
SabrinaSusmi <sabrina.su512@gmail.com>
2020-08-07 03:05:18 +0600

    AI Move method added to classic theme #6

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ClassicTheme.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ClassicTheme.java
@@ -8,16 +8,13 @@ import java.awt.event.ActionListener;
 public class ClassicTheme {
     private JPanel panel;
     private JButton button[] = new JButton[9];
-    private Font font;
+    private Font font = new Font("Arial", Font.BOLD, 30);
 
     public ClassicTheme(JPanel panel) {
         this.panel = panel;
     }
 
     public JPanel drawClassicTheme() {
-
-        font = new Font("Arial", Font.BOLD, 30);
-
         panel.setLayout(new GridLayout(3,3));
 
         for (int i=0; i<9; i++) {
@@ -27,21 +24,37 @@ public class ClassicTheme {
             panel.add(button[i]);
         }
 
-        for (JButton button: button
-             ) {
-            button.addActionListener(new ActionListener() {
+
+        AIMove(button);
+
+
+        return panel;
+    }
+
+    private void playerMove(JButton button[]){
+        for (JButton b: button
+        ) {
+            b.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if(button.getText()==""){
-                        button.setText("X");
-                        button.setFont(font);
+                    if(b.getText()==""){
+                        b.setText("X");
+                        b.setFont(font);
                     }
                 }
             });
 
         }
+    }
 
-
-        return panel;
+    private void AIMove(JButton button[]){
+        int index = (int)Math.random()%8;
+        if(button[index].getText()==""){
+            button[index].setText("O");
+            button[index].setFont(font);
+        }
+        else {
+            AIMove(button);
+        }
     }
 }
