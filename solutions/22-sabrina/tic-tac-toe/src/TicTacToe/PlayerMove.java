915ed296ab5883f71e310660290013ae6dfa365a
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-11 20:53:11 +0600

    winner in a single function #1  #14

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/PlayerMove.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/PlayerMove.java
@@ -1,12 +1,10 @@
 package TicTacToe;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PlayerMove {
-    private JPanel panel;
     private JButton button[][];
     private String currentPlayer = "x";
     private int moveCount=0;
@@ -18,31 +16,18 @@ public class PlayerMove {
 
     private boolean isWon(){
         for(int i=0; i<3; i++){
-            if(button[i][0].getText()=="x" && button[i][1].getText()=="x" && button[i][2].getText()=="x"){
+            if(button[i][0].getText()==button[i][1].getText() && button[i][1].getText()==button[i][2].getText()
+                 && (button[i][0].getText()=="x"||button[i][0].getText()=="o")){
                 return true;
             }
-            else if(button[0][i].getText()=="x" && button[1][i].getText()=="x" && button[2][i].getText()=="x"){
-                return true;
-            }
-        }
-        if((button[0][0].getText()=="x" && button[1][1].getText()=="x" && button[2][2].getText()=="x")
-                || (button[0][2].getText()=="x" && button[1][1].getText()=="x" && button[2][0].getText()=="x")){
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isWonAI(){
-        for(int i=0; i<3; i++){
-            if(button[i][0].getText()=="o" && button[i][1].getText()=="o" && button[i][2].getText()=="o"){
-                return true;
-            }
-            else if(button[0][i].getText()=="o" && button[1][i].getText()=="o" && button[2][i].getText()=="o"){
+            else if(button[0][i].getText()==button[1][i].getText() && button[1][i].getText()==button[2][i].getText()
+                && (button[0][i].getText()=="x"||button[0][i].getText()=="o")){
                 return true;
             }
         }
-        if((button[0][0].getText()=="o" && button[1][1].getText()=="o" && button[2][2].getText()=="o")
-                || (button[0][2].getText()=="o" && button[1][1].getText()=="o" && button[2][0].getText()=="o")){
+        if((button[0][0].getText()==button[1][1].getText() && button[1][1].getText()==button[2][2].getText())
+                || (button[0][2].getText()==button[1][1].getText() && button[1][1].getText()==button[2][0].getText())
+            && (button[1][1].getText()=="x"||button[1][1].getText()=="o")){
             return true;
         }
         return false;
@@ -86,7 +71,7 @@ public class PlayerMove {
     private void togglePlayer(){
         if(currentPlayer=="o"){
             randomAI.move(button);
-            if(isWonAI()==true){
+            if(isWon()==true){
                 newBoard();
                 moveCount=0;
             }
