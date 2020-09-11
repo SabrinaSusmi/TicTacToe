ff6b937fd03853fc2c787f1890bf2e99531a2f7c
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-11 21:03:12 +0600

    winner function changes updated in startGame #7

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/StartGame.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/StartGame.java
@@ -35,6 +35,33 @@ public class StartGame {
         currentPlayer="x";
     }
 
+    private boolean isWon(){
+        for(int i=0; i<3; i++){
+            if(button[i][0].getText()==button[i][1].getText() && button[i][1].getText()==button[i][2].getText()
+                    && (button[i][0].getText()=="x"||button[i][0].getText()=="o")){
+                return true;
+            }
+            else if(button[0][i].getText()==button[1][i].getText() && button[1][i].getText()==button[2][i].getText()
+                    && (button[0][i].getText()=="x"||button[0][i].getText()=="o")){
+                return true;
+            }
+        }
+        if((button[0][0].getText()==button[1][1].getText() && button[1][1].getText()==button[2][2].getText())
+                || (button[0][2].getText()==button[1][1].getText() && button[1][1].getText()==button[2][0].getText())
+                && (button[1][1].getText()=="x"||button[1][1].getText()=="o")){
+            return true;
+        }
+        return false;
+    }
+
+    private void newBoard(){
+        for (int i=0; i<3; i++) {
+            for (int j = 0; j < 3; j++) {
+                button[i][j].setText("");
+            }
+        }
+    }
+
     public void move(){
         for (int i=0; i<3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -45,10 +72,18 @@ public class StartGame {
                     public void actionPerformed(ActionEvent e) {
                         if (((JButton) e.getSource()).getText().equals("") && currentPlayer == "x"){
                             playersMove(button[row][col]);
+                            if(isWon()==true){
+                                newBoard();
+                                moveCount=0;
+                            }
                             currentPlayer = "o";
                             moveCount++;
                             if (moveCount <= 8) {
                                 computersMove();
+                                if(isWon()==true){
+                                    newBoard();
+                                    moveCount=0;
+                                }
                             }
                         }
                     }
