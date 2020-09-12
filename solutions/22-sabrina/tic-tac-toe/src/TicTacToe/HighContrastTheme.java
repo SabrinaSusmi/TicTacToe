5e3fe0e7ed1171d3d8b0f574c8484f62a3ffc685
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-13 01:32:37 +0600

    highContrast theme property. Doesnt work properly #11

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/HighContrastTheme.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/HighContrastTheme.java
@@ -16,8 +16,18 @@ public class HighContrastTheme implements Theme{
     public void drawTheme() {
         for (int i=0; i<3; i++) {
             for (int j = 0; j < 3; j++) {
-                button[i][j].setBackground(new Color(0x69696f));
                 button[i][j].setBorder(BorderFactory.createLineBorder(new Color(0xc0c0c0), 2));
+                if(button[i][j].getText()=="x"){
+                    button[i][j].setBackground(Color.black);
+                    button[i][j].setForeground(Color.WHITE);
+                }
+                else if(button[i][j].getText()=="o"){
+                    button[i][j].setBackground(Color.white);
+                    button[i][j].setForeground(Color.black);
+                }
+                else{
+                    button[i][j].setBackground(new Color(0x69696f));
+                }
             }
         }
     }
