18349f549d2be5742b604deb2d5504aea699c752
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-18 01:38:33 +0600

    playgame action listener simplified #1

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/PlayGame.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/PlayGame.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 public class PlayGame {
     public JButton button[][] = new JButton[3][3];
     String moveString[][]= new String[3][3];
-    private String currentPlayer = "x";
     AI ai;
     Move move = new Move();
     HasWinner hasWinner = new HasWinner();
@@ -36,7 +35,6 @@ public class PlayGame {
     }
 
     public void move(){
-        newBoard();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 int row = i;
@@ -44,49 +42,37 @@ public class PlayGame {
                 button[i][j].addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        if (((JButton) e.getSource()).getText().equals("")) {
-                            move.setPlayerMove(button[row][col]);
-                            moveCount++;
-                            moveString[row][col] = "x";
-                            if (hasWinner.isWon("x", moveString) == true) {
-                                moveCount=0;
-                                winnerMessage(currentPlayer);
-                                newBoard();
-                            } else {
-                                currentPlayer = "o";
-                                if (isDraw()==true) {
+                        if (e.getSource().equals(button[row][col])) {
+                            if(button[row][col].getText()==""){
+                                move.setPlayerMove(button[row][col]);
+                                moveString[row][col] = "x";
+                                moveCount++;
+                                if (hasWinner.isWon("x", moveString) == true){
+                                    moveCount=0;
+                                    winnerMessage("x");
+                                    newBoard();
+                                }
+                                if(moveCount>=1){
                                     move.setComputerMove(ai, button, moveString);
                                     moveCount++;
-                                    if (hasWinner.isWon("o", moveString) == true) {
-                                        moveCount=0;
-                                        winnerMessage(currentPlayer);
-                                        newBoard();
-                                    } else {
-                                        currentPlayer = "x";
-                                    }
-                                } else {
+                                }
+                                if (hasWinner.isWon("o", moveString) == true){
+                                    moveCount=0;
+                                    winnerMessage("o");
+                                    newBoard();
+                                }
+                                if(moveCount==9){
                                     moveCount=0;
                                     winnerMessage("draw");
                                     newBoard();
                                 }
+
                             }
                         }
                     }
                 });
             }
         }
-    }
-
-    private boolean isDraw(){
-        boolean flag=false;
-        for (int i=0; i<3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if(button[i][j].getText()==""){
-                    flag=true;
-                }
-            }
-        }
-        return flag;
     }
 
     private void winnerMessage(String currentPlayer){
