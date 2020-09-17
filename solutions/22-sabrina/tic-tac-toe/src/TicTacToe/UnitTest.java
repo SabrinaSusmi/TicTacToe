d447625f2a9ea8794652debd7d23c96cefac272f
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-18 02:00:26 +0600

    first unit testing #1

---
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/UnitTest.java
@@ -0,0 +1,21 @@
+package TicTacToe;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class UnitTest {
+    @Test
+    public void firstRowWinnerCheck(){
+        String[][] strings = new String[3][3];
+        boolean won;
+        HasWinner hasWinner = new HasWinner();
+
+        strings[0][0]="x";
+        strings[1][1]="o";
+        strings[0][1]="x";
+        strings[2][0]="o";
+        strings[0][2]="x";
+        won = hasWinner.isWon("x",strings);
+        Assertions.assertTrue(won);
+    }
+}
