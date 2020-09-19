cf17f723637109dcd7893c791e759e4be4107352
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-19 23:12:18 +0600

    defensive AI #13

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/DefensiveMoveChecker.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/DefensiveMoveChecker.java
@@ -10,10 +10,13 @@ public class DefensiveMoveChecker {
     public boolean checkDefenseRow(){
         for(int i=0; i<3; i++){
             if (moveString[i][0] == moveString[i][1] && moveString[i][0] == "x" && moveString[i][2]=="") {
+                moveString[i][2]="defend";
                 return true;
             } else if (moveString[i][1] == moveString[i][2] && moveString[i][1] == "x" && moveString[i][0]=="") {
+                moveString[i][0]="defend";
                 return true;
             } else if(moveString[i][0] == moveString[i][2] && moveString[i][0] == "x" && moveString[i][1]==""){
+                moveString[i][1]="defend";
                 return true;
             }
         }
@@ -23,10 +26,13 @@ public class DefensiveMoveChecker {
     public boolean checkDefenseColumn(){
         for(int i=0; i<3; i++){
             if (moveString[0][i] == moveString[1][i] && moveString[0][i] == "x" && moveString[2][i]=="") {
+                moveString[2][i]="defend";
                 return true;
             } else if (moveString[1][i] == moveString[2][i] && moveString[1][i] == "x" && moveString[0][i]=="") {
+                moveString[0][i]="defend";
                 return true;
             } else if(moveString[0][i] == moveString[2][i] && moveString[0][i] == "x" && moveString[1][i]==""){
+                moveString[1][i]="defend";
                 return true;
             }
         }
@@ -36,10 +42,13 @@ public class DefensiveMoveChecker {
     public boolean checkDefenseFirstDiagonal() {
         for (int i = 0; i < 3; i++) {
             if(moveString[0][0]==moveString[1][1] && moveString[1][1]=="x" && moveString[2][2]==""){
+                moveString[2][2]="defend";
                 return true;
             } else if(moveString[2][2]==moveString[1][1] && moveString[1][1]=="x" && moveString[0][0]==""){
+                moveString[0][0]="defend";
                 return true;
             } else if(moveString[0][0]==moveString[2][2] && moveString[0][0]=="x" && moveString[1][1]==""){
+                moveString[1][1]="defend";
                 return true;
             }
         }
@@ -49,13 +58,24 @@ public class DefensiveMoveChecker {
     public boolean checkDefenseSecondDiagonal() {
         for (int i = 0; i < 3; i++) {
             if(moveString[0][2]==moveString[1][1] && moveString[1][1]=="x" && moveString[2][0]==""){
+                moveString[2][0]="defend";
                 return true;
             } else if(moveString[2][0]==moveString[1][1] && moveString[1][1]=="x" && moveString[0][2]==""){
+                moveString[0][2]="defend";
                 return true;
             } else if(moveString[0][2]==moveString[2][0] && moveString[0][2]=="x" && moveString[1][1]==""){
+                moveString[1][1]="defend";
                 return true;
             }
         }
         return false;
+    }
+
+    public boolean canBeDefended(){
+        boolean flag=false;
+        if(checkDefenseRow()||checkDefenseColumn()||checkDefenseFirstDiagonal()||checkDefenseSecondDiagonal()){
+            flag=true;
+        }
+        return true;
     }
 }
