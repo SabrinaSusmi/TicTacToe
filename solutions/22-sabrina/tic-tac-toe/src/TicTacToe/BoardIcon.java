a1d7f85bea58a4bf46559b9fd26bba1134d1bfc3
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-21 00:37:42 +0600

    New class created to get all icons in single class #9 #10 #11

---
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/BoardIcon.java
@@ -0,0 +1,50 @@
+package TicTacToe;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class BoardIcon {
+    private String classicPlayerIconPath = "src/TicTacToe/imageIcons/classicThemePlayerIcon.png";
+    private String classicAIIconPath = "src/TicTacToe/imageIcons/classicThemeAiIcon.jpg";
+    private String forestPlayerIconPath = "src/TicTacToe/imageIcons/forestThemePlayerIcon.png";
+    private String forestAIIconPath = "src/TicTacToe/imageIcons/forestThemeAiIcon.png";
+    private String highContrastPlayerIconPath = "src/TicTacToe/imageIcons/highContrastThemePlayerIcon.png";
+    private String highContrastAIIconPath = "src/TicTacToe/imageIcons/highContrastThemeAiIcon.jpg";
+
+    public Icon getIcon(String path){
+        Image image = null;
+        try {
+            image = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            System.out.println(e+" Image failed");
+        }
+        return new ImageIcon(image.getScaledInstance(120,120,Image.SCALE_AREA_AVERAGING));
+    }
+
+    public Icon getClassicThemePlayerIcon(){
+        return getIcon(classicPlayerIconPath);
+    }
+
+    public Icon getClassicThemeAIIcon(){
+        return getIcon(classicAIIconPath);
+    }
+
+    public Icon getForestThemePlayerIcon(){
+        return getIcon(forestPlayerIconPath);
+    }
+
+    public Icon getForestThemeAIIcon(){
+        return getIcon(forestAIIconPath);
+    }
+
+    public Icon getHighContrastThemePlayerIcon(){
+        return getIcon(highContrastPlayerIconPath);
+    }
+
+    public Icon getHighContrastThemeAIIcon(){
+        return getIcon(highContrastAIIconPath);
+    }
+}
