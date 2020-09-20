35f6c9083d9044b02c44577856944df3844e04a3
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-20 16:10:09 +0600

    Theme colors set in single loop #8

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ApplyTheme.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/ApplyTheme.java
@@ -16,7 +16,8 @@ public class ApplyTheme {
         theme = selectTheme.getTheme();
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
-                buttonArray[i][j].setBackground(null);
+                buttonArray[i][j].setBackground(theme.getBackgroundColor());
+                buttonArray[i][j].setBorder(BorderFactory.createLineBorder(theme.getBorderColor(), 2));
             }
         }
     }
