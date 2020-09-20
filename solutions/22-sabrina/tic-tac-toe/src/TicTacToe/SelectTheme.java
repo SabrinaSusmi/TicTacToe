9eb218965f35da611aea3383f463f5493ca6f9bc
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-20 15:12:14 +0600

    new class created to set and get theme #9 #10 #11

---
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/SelectTheme.java
@@ -0,0 +1,12 @@
+package TicTacToe;
+
+public class SelectTheme {
+    Theme theme;
+    public void setTheme(Theme theme){
+        this.theme=theme;
+    }
+
+    public Theme getTheme(){
+        return theme;
+    }
+}
