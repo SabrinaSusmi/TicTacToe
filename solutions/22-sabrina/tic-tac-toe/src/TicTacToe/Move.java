e38a56e4ed260be7b60c638c0dfff70caef1ebfb
SabrinaSusmi <sabrina.su512@gmail.com>
2020-09-21 13:28:24 +0600

    dead code removed #1

--- solutions/22-sabrina/tic-tac-toe/src/TicTacToe/Move.java
+++ solutions/22-sabrina/tic-tac-toe/src/TicTacToe/Move.java
@@ -15,13 +15,11 @@ public class Move {
     }
 
     void setPlayerMove(JButton button){
-        //button.setText("x");
         theme = selectTheme.getTheme();
         button.setIcon(theme.getPlayerIcon());
     }
 
     void setComputerMove(AI ai, JButton[][] button, String[][] moveString){
-        //ai = selectAI.getAi();
         ai.move(button,moveString);
     }
 }
